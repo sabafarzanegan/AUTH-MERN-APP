96bbf935c2fcf65e04c3a4429e708f63a95ee918
saba farzanegan <saba.far.99@gmail.com>
2024-05-02 16:51:26 +0330

    connect signup page to backend

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -1,18 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Signup() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const signupInpuHandler = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      setError(false);
+      const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+
+      setLoading(false);
+    } catch (error) {
+      // setLoading(false);
+      // setError(true);
+    }
+  };
+
   return (
     <>
       {/* title component */}
       <h1 className="font-lalezar text-3xl text-primary text-center mt-6">
         ثبت نام
       </h1>
+      {/* error section */}
+      {error && (
+        <div className=" w-[30%] m-auto mt-2 border border-red-600 bg-red-600 text-white text-center font-vazir p-4 ">
+          <p>خطا در ارسال اطلاعات</p>
+        </div>
+      )}
+
       {/* signup container */}
       <div className="w-full flex items-center justify-center mt-10 rounded-md ">
         {/* signup content */}
         <div className="md:w-[40%] p-10 bg-third">
-          <form action="" className="flex flex-col  justify-center gap-y-5">
+          <form
+            action=""
+            onSubmit={handelSubmit}
+            className="flex flex-col  justify-center gap-y-5">
             <label className="text-right font-vazir text-white text-sm px-1">
               نام کاربری
             </label>
@@ -21,6 +64,7 @@ function Signup() {
               id="username"
               className="p-3 rounded-lg"
               placeholder="username"
+              onChange={signupInpuHandler}
             />
             <label className="text-right font-vazir text-white text-sm px-1">
               ایمیل
@@ -31,6 +75,7 @@ function Signup() {
               id="email"
               className="p-3 rounded-lg border-none outline-none"
               placeholder="email"
+              onChange={signupInpuHandler}
             />
             <label className="text-right font-vazir text-white text-sm px-1 border-none outline-none">
               رمزعبور
@@ -40,11 +85,13 @@ function Signup() {
               id="password"
               className="p-3 rounded-lg border-none outline-none"
               placeholder="password"
+              onChange={signupInpuHandler}
             />
             <button
               type="submit"
+              disabled={loading}
               className="w-full bg-primary p-3 text-white font-vazir text-xl mt-2  rounded-lg">
-              ثبت نام
+              {loading ? "...درحال ارسال" : "ثبت نام"}
             </button>
           </form>
           <p className="text-center text-white font-vazir text-xl mt-6 ">
